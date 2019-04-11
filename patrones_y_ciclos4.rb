33d9a1e43b16eef678e40fcbfec4f586152d9ecc
Carolina <carolinaevevarela@gmail.com>
2019-04-10 22:01:55 -0400

    Patrones y ciclos 2

--- patrones_y_ciclos4.rb
+++ patrones_y_ciclos4.rb
@@ -1,10 +1,11 @@
 n = ARGV[0].to_i
 n.times do |i|
-  if i.even?
-    print "1"
-  else
-    print "2"
-    print "3"
+    if i%4 == 0 || i%4 == 1 || i%4 == 2
+      print "1"
+    elsif
+      print "2"
+    elsif
+      print "3"
   end
 end
 
